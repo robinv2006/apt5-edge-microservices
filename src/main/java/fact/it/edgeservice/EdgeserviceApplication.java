6f81df5005558b3c1961dc803dcc9a2b0e146e04
robinv2006 <r0740489@student.thomasmore.be>
2020-11-03 19:12:57 +0100

    initial commit

---
+++ src/main/java/fact/it/edgeservice/EdgeserviceApplication.java
@@ -0,0 +1,13 @@
+package fact.it.edgeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdgeserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdgeserviceApplication.class, args);
+    }
+
+}
