59361743412888d55ac25d7c9c0adcedecb05c2b
Amber Akkermans <r0740967@student.thomasmore.be>
2020-11-18 20:01:31 +0100

    get unit tests werkend

--- src/test/java/fact/it/edgeservice/BookingControllerUnitTests.java
+++ src/test/java/fact/it/edgeservice/BookingControllerUnitTests.java
@@ -71,6 +71,7 @@ public class BookingControllerUnitTests {
     private List<Hotel> allHotels = Arrays.asList(hotel1, hotel2);
     private List<Employee> allEmployeesHotel1 = Arrays.asList(employee1);
     private List<Customer> allCustomersHotel1 = Arrays.asList(customer1);
+    private List<Customer> allCustomersRoom1 = Arrays.asList(customer1);
 
     @BeforeEach
     public void initializeMockserver() {
@@ -79,33 +80,94 @@ public class BookingControllerUnitTests {
 
     @Test
     public void whenGetRoomsByHotelCode_thenReturnRoomsJson() throws Exception {
+        // GET all rooms from Hotel 1
+        mockServer.expect(ExpectedCount.once(),
+                requestTo(new URI("http://" + roomServiceBaseUrl + "/hotels/H01")))
+                .andExpect(method(HttpMethod.GET))
+                .andRespond(withStatus(HttpStatus.OK)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(mapper.writeValueAsString((allRoomsFromHotel1))));
 
-//        // GET all rooms from Hotel 1
-//        mockServer.expect(ExpectedCount.once(),
-//                requestTo(new URI("http://" + hotelServiceBaseUrl + "/hotels/H01")))
-//                .andExpect(method(HttpMethod.GET))
-//                .andRespond(withStatus(HttpStatus.OK)
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .body(mapper.writeValueAsString(allHotels))
-//                );
-//
-//        // GET info from Room 1
-//        mockServer.expect(ExpectedCount.once(),
-//                requestTo(new URI("http://" + roomServiceBaseUrl + "/hotel/H01")))
-//                .andExpect(method(HttpMethod.GET))
-//                .andRespond(withStatus(HttpStatus.OK)
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .body(mapper.writeValueAsString(allRoomsFromHotel1))
-//                );
-//
-//        mockMvc.perform(get("/bookings/rooms/{hotelCode}", "H01"))
-//                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$", hasSize(1)))
-//                .andExpect(jsonPath("$[0].roomCode", is("R01")))
-//                .andExpect(jsonPath("$[0].hotelCode", is("H01")))
-//                .andExpect(jsonPath("$[0].number", is(1)))
-//                .andExpect(jsonPath("$[0].price", is(15.0)))
-//                .andExpect(jsonPath("$[0].description", is("goedkoop kamerke")));
+
+        mockMvc.perform(get("/bookings/rooms/{hotelCode}", "H01"))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].roomCode", is("R01")))
+                .andExpect(jsonPath("$[0].hotelCode", is("H01")))
+                .andExpect(jsonPath("$[0].number", is(1)))
+                .andExpect(jsonPath("$[0].price", is(15.0)))
+                .andExpect(jsonPath("$[0].description", is("goedkoop kamerke")));
+    }
+
+    @Test
+    public void whenGetHotels_thenReturnHotelsJson() throws Exception{
+        mockServer.expect(ExpectedCount.once(),
+                requestTo(new URI("http://" + hotelServiceBaseUrl + "/hotels")))
+                .andExpect(method(HttpMethod.GET))
+                .andRespond(withStatus(HttpStatus.OK)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(mapper.writeValueAsString((allHotels))));
+
+        mockMvc.perform(get("/bookings/hotels"))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].hotelCode", is("H01")))
+                .andExpect(jsonPath("$[0].name", is("Sprinton Palace")))
+                .andExpect(jsonPath("$[0].city", is("Geel")))
+                .andExpect(jsonPath("$[0].street", is("Geelsebaan")))
+                .andExpect(jsonPath("$[0].number", is(52)))
+                .andExpect(jsonPath("$[1].hotelCode", is("H02")))
+                .andExpect(jsonPath("$[1].name", is("Hotelleke aan zee")))
+                .andExpect(jsonPath("$[1].city", is("Oostende")))
+                .andExpect(jsonPath("$[1].street", is("Oostendsebaan")))
+                .andExpect(jsonPath("$[1].number", is(12)));
+    }
+
+    @Test
+    public void whenGetEmployeesByHotelCode_thenReturnEmployeesJson() throws Exception{
+        mockServer.expect(ExpectedCount.once(),
+                requestTo(new URI("http://" + employeeServiceBaseUrl + "/employees/H01")))
+                .andExpect(method(HttpMethod.GET))
+                .andRespond(withStatus(HttpStatus.OK)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(mapper.writeValueAsString((allEmployeesHotel1))));
+
+        mockMvc.perform(get("/bookings/employees/{hotelCode}", "H01"))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].employeeCode", is("E01")))
+                .andExpect(jsonPath("$[0].hotelCode", is("H01")))
+                .andExpect(jsonPath("$[0].firstName", is("Emp")))
+                .andExpect(jsonPath("$[0].lastName", is("Loyee")))
+                .andExpect(jsonPath("$[0].function", is("Barman")));
+    }
+
+    @Test
+    public void whenGetCustomersByHotelCode_thenReturnCustomersJson() throws Exception{
+        mockServer.expect(ExpectedCount.once(),
+                requestTo(new URI("http://" + roomServiceBaseUrl + "/hotels/H01")))
+                .andExpect(method(HttpMethod.GET))
+                .andRespond(withStatus(HttpStatus.OK)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(mapper.writeValueAsString((allRoomsFromHotel1))));
+
+        mockServer.expect(ExpectedCount.once(),
+                requestTo(new URI("http://" + customerServiceBaseUrl + "/customers/rooms/R01")))
+                .andExpect(method(HttpMethod.GET))
+                .andRespond(withStatus(HttpStatus.OK)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(mapper.writeValueAsString((allCustomersRoom1))));
+
+        mockMvc.perform(get("/bookings/customers/{hotelCode}", "H01"))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].customerCode", is("C01")))
+                .andExpect(jsonPath("$[0].roomCode", is("R01")))
+                .andExpect(jsonPath("$[0].firstName", is("Jeff")))
+                .andExpect(jsonPath("$[0].lastName", is("Jeffen")))
+                .andExpect(jsonPath("$[0].city", is("Antwerpen")))
+                .andExpect(jsonPath("$[0].street", is("Straat1")))
+                .andExpect(jsonPath("$[0].number", is(1)));
     }
 }
