28ad2c8b9f23cb56142217ad305fd70beaefb697
Tim Keuppens <r0756055@student.thomasmore.be>
2020-12-02 16:33:22 +0100

    adding yamls and startup script

---
+++ Kubernetes/KubernetesRun.ps1
@@ -0,0 +1,17 @@
+﻿# Om op te starten in PS (admin)
+# & ".\KubernetesRun.ps1"
+
+
+# minikube delete
+# minikube start
+& minikube -p minikube docker-env | Invoke-Expression
+docker stop $(docker ps -aq)
+docker rm $(docker ps -aq) 
+
+
+
+kubectl apply -f deploy-room.yaml
+kubectl apply -f deploy-edge.yaml
+
+kubectl get services
+kubectl get deployment
